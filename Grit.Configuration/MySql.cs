765afbdc1ac301e82603cd8b0491bc361a2c05d6
weixiao <zhongwx@gmail.com>
2014-10-14 17:51:00 +0800

    sql

--- Grit.Configuration/MySql.cs
+++ Grit.Configuration/MySql.cs
@@ -8,7 +8,7 @@ namespace Grit.Configuration
 {
     public static class MySql
     {
-        public static readonly string Sequence = "Server=localhost;Port=3306;Database=grit;Uid=root;Pwd=flowers;";
-        public static readonly string RBAC = "Server=localhost;Port=3306;Database=grit;Uid=root;Pwd=flowers;";
+        public static readonly string Sequence = "Server=localhost;Port=3306;Database=grit;Uid=root;Pwd=123456;";
+        public static readonly string RBAC = "Server=localhost;Port=3306;Database=grit;Uid=root;Pwd=123456;";
     }
 }
