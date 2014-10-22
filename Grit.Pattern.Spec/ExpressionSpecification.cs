26a8fa00571ad5c1192205ee24b2448125a5203b
weixiao <zhongwx@gmail.com>
2014-10-22 14:09:31 +0800

    spec

---
+++ Grit.Pattern.Spec/ExpressionSpecification.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grit.Pattern.Spec
+{
+    public class ExpressionSpecification<T> : CompositeSpecification<T>
+    {
+        private Func<T, bool> expression;
+        private string UnsatisfiedMessage;
+        public ExpressionSpecification(Func<T, bool> expression, string unsatisfiedMessage = null)
+        {
+            if (expression == null)
+                throw new ArgumentNullException();
+            else
+                this.expression = expression;
+
+            this.UnsatisfiedMessage = unsatisfiedMessage;
+        }
+
+        public override bool IsSatisfiedBy(T o)
+        {
+            return this.expression(o);
+        }
+    }
+}
