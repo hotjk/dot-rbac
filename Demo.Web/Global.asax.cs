12782b13e6bc51543bee5a7ba411b81e234c9c9a
weixiao <zhongwx@gmail.com>
2014-11-26 11:23:53 +0800

    remove x-headers

--- Demo.Web/Global.asax.cs
+++ Demo.Web/Global.asax.cs
@@ -18,6 +18,16 @@ namespace Demo.Web
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            //HttpContext.Current.Response.Headers.Remove("X-Powered-By");
+            //HttpContext.Current.Response.Headers.Remove("X-AspNet-Version");
+            //HttpContext.Current.Response.Headers.Remove("X-AspNetMvc-Version");
+            HttpContext.Current.Response.Headers.Remove("Server");
         }
     }
 }
