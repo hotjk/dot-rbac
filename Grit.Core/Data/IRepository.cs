8cd0c91384e9781cac4df9d3a565b355c1b9cbd9
weixiao <zhongwx@gmail.com>
2014-12-02 15:20:58 +0800

    grit.core

---
+++ Grit.Core/Data/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grit.Core.Data
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+        T GetById(object id);
+    }
+}
