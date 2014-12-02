341966d6cbdc33a5fdbbab39030c3c26b7c33ef7
weixiao <zhongwx@gmail.com>
2014-12-02 17:45:08 +0800

    Grit.Data.Repository

---
+++ Grit.Data/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grit.Core.Data;
+using Grit.Core;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+namespace Grit.Data
+{
+    public abstract class Repository<T> : IRepository<T>
+        where T : BaseEntity
+    {
+        protected virtual string Table { get; private set; }
+
+        public Repository()
+        {
+            this.Table = typeof(T).Name;
+        }
+
+        protected abstract IDbConnection OpenConnection();
+
+        public virtual T GetById(object id)
+        {
+            using (IDbConnection connection = OpenConnection())
+            {
+                return connection.Query<T>(
+                    "SELECT * FROM @Table WHERE Id=@Id;",
+                    new { Table = Table, Id = (int)id }).SingleOrDefault();
+            }
+        }
+    }
+}
