c207b5ec0d83bf7104be666f99a9bc1c79251171
weixiao <zhongwx@gmail.com>
2015-01-15 15:42:36 +0800

    encryptkey

---
+++ Settings.Web/Controllers/EncryptKeyController.cs
@@ -0,0 +1,25 @@
+﻿using Grit.Sequence;
+using Grit.Tree.JsTree;
+using Grit.Utility.Security;
+using Grit.Utility.Web.Json;
+using Settings.Model;
+using Settings.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Settings.Web.Controllers
+{
+    public class EncryptKeyController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            string publicKey, privateKey;
+            RSAManager.GenerateKeyAndIV(out publicKey, out privateKey);
+            return View(new EncryptKeyVM { PublicKey = publicKey, PrivateKey = privateKey });
+        }
+   }
+}
---
+++ Settings.Web/Models/EncryptKeyVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Settings.Web.Models
+{
+    public class EncryptKeyVM
+    {
+        [Display(Name = "公钥")]
+        public string PublicKey { get; set; }
+
+        [Display(Name = "私钥")]
+        public string PrivateKey { get; set; }
+    }
+}
