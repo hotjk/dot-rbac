a13203c28871a8d3f29a7e6ebaed09e70b2c54f0
weixiao <zhongwx@gmail.com>
2015-02-12 10:46:31 +0800

    script update

--- Dump20150212.sql
+++ Dump20150212.sql
@@ -1,8 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `grit` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `grit`;
+CREATE DATABASE  IF NOT EXISTS `rbac` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `rbac`;
 -- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
 --
--- Host: 127.0.0.1    Database: grit
+-- Host: 127.0.0.1    Database: rbac
 -- ------------------------------------------------------
 -- Server version	5.6.19
 
@@ -85,32 +85,8 @@ CREATE TABLE `rbac_role_permission` (
 
 LOCK TABLES `rbac_role_permission` WRITE;
 /*!40000 ALTER TABLE `rbac_role_permission` DISABLE KEYS */;
-INSERT INTO `rbac_role_permission` VALUES (1,2),(1,3),(1,4),(1,22),(2,2),(2,4),(2,10),(2,20),(3,1),(3,2),(3,3);
+INSERT INTO `rbac_role_permission` VALUES (1,2),(1,3),(1,4),(2,1),(2,2),(2,21),(3,1),(3,2),(4,10),(4,20),(4,22),(5,20),(5,22),(6,10);
 /*!40000 ALTER TABLE `rbac_role_permission` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `rbac_subject_role`
---
-
-DROP TABLE IF EXISTS `rbac_subject_role`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rbac_subject_role` (
-  `SubjectId` int(11) NOT NULL,
-  `RoleId` int(11) NOT NULL,
-  PRIMARY KEY (`SubjectId`,`RoleId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rbac_subject_role`
---
-
-LOCK TABLES `rbac_subject_role` WRITE;
-/*!40000 ALTER TABLE `rbac_subject_role` DISABLE KEYS */;
-INSERT INTO `rbac_subject_role` VALUES (1,1),(2,1),(2,2),(3,1),(3,2),(3,3),(7,1),(7,5);
-/*!40000 ALTER TABLE `rbac_subject_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -157,9 +133,63 @@ CREATE TABLE `rbac_subject_permission` (
 
 LOCK TABLES `rbac_subject_permission` WRITE;
 /*!40000 ALTER TABLE `rbac_subject_permission` DISABLE KEYS */;
-INSERT INTO `rbac_subject_permission` VALUES (1,21),(2,20),(7,2),(7,3);
+INSERT INTO `rbac_subject_permission` VALUES (1,21),(2,20),(5,22),(7,2),(7,3);
 /*!40000 ALTER TABLE `rbac_subject_permission` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `rbac_subject_role`
+--
+
+DROP TABLE IF EXISTS `rbac_subject_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rbac_subject_role` (
+  `SubjectId` int(11) NOT NULL,
+  `RoleId` int(11) NOT NULL,
+  PRIMARY KEY (`SubjectId`,`RoleId`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rbac_subject_role`
+--
+
+LOCK TABLES `rbac_subject_role` WRITE;
+/*!40000 ALTER TABLE `rbac_subject_role` DISABLE KEYS */;
+INSERT INTO `rbac_subject_role` VALUES (1,1),(2,1),(2,2),(3,1),(3,2),(3,3),(5,1),(5,3),(5,4),(7,1),(7,5);
+/*!40000 ALTER TABLE `rbac_subject_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tree`
+--
+
+DROP TABLE IF EXISTS `tree`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tree` (
+  `Tree` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
+  `Parent` int(11) DEFAULT NULL,
+  `Data` int(11) DEFAULT NULL,
+  PRIMARY KEY (`Id`,`Tree`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tree`
+--
+
+LOCK TABLES `tree` WRITE;
+/*!40000 ALTER TABLE `tree` DISABLE KEYS */;
+INSERT INTO `tree` VALUES (6,0,NULL,NULL),(7,0,NULL,NULL),(8,0,NULL,NULL),(6,1,0,2),(7,1,0,1),(8,1,0,5),(6,2,1,5),(7,2,1,4),(8,2,1,1),(6,3,1,7),(7,3,2,3),(8,3,1,2),(6,4,0,3),(7,4,0,2),(8,4,1,21),(6,5,4,4),(7,5,0,5),(8,5,1,4),(6,6,4,1),(7,6,0,6),(8,6,1,3),(6,7,0,6),(8,7,0,10),(6,8,0,8),(8,8,0,20),(8,9,0,22);
+/*!40000 ALTER TABLE `tree` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'rbac'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -170,4 +200,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2015-02-11 23:08:31
+-- Dump completed on 2015-02-12 10:46:02
